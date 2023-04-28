b2abf2539b82daffca5eddbf836b51ac17c9f7ba
dasumo <dasumo@zend-dasumo.fjeclot.net>
2023-04-28 17:44:55 +0200

    Prueba CSS

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>
-			PÀGINA WEB INICIAL DE L'APLICACIÓ
-		</title>
-	</head>
-	<body>
-		<h1> APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h1>
-		<h2> DAW2 M08UF2 M08UF3 </h2>
-		<h3> Autor: dasumo2023daw2</h3>
-		<h3> Correu: davsun22daw@proton.me</h3>
-		<h3> Github: https://github.com/davsun22daw/daw2_m08uf23_projecte_suner_david.git</h3>
-		<a href="http://zend-dasumo.fjeclot.net/projecte/login.php">Inicia sessió</a>
-	</body>
+<head>
+	<title>PÀGINA WEB INICIAL DE L'APLICACIÓ</title>
+	<!-- Enlazar los archivos CSS y JavaScript de Bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<h1 class="display-4 mt-5">APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h1>
+		<h2 class="my-4">DAW2 M08UF2 M08UF3</h2>
+		<h3>Autor: dasumo2023daw2</h3>
+		<h3>Correu: davsun22daw@proton.me</h3>
+		<h3>GitHub: <a href="https://github.com/davsun22daw/daw2_m08uf23_projecte_suner_david.git">https://github.com/davsun22daw/daw2_m08uf23_projecte_suner_david.git</a></h3>
+		<a href="http://zend-dasumo.fjeclot.net/projecte/login.php" class="btn btn-primary my-4">Inicia sessió</a>
+	</div>
+</body>
 </html>
